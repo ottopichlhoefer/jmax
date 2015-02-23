df1c10501ed76fe7d8e32ecbc82dc5cc6cae458a
ottopichlhoefer <otto.pichlhoefer@nordrandklinik.at>
2015-02-23 07:45:14 +0100

    batch files

---
+++ jmax --debug-init.bat
@@ -0,0 +1,4 @@
+REM set PATH=%CD%\gnutls-3.2.15-w32\bin;%CD%\Aspell-win32\bin;%PATH%
+set PATH=%CD%\gnutls-3.2.15-w32\bin;%PATH%
+REM start emacs-24.3\bin\runemacs.exe -l init.el %1
+start emacs-24.3\bin\runemacs.exe --debug-init
