5eae28ac0a902d2f38ef36d7dc64509215496371
ottopichlhoefer <otto.pichlhoefer@nordrandklinik.at>
2015-07-11 16:19:48 +0200

    adapted path

--- jmax - emacs.bat
+++ jmax - emacs.bat
@@ -1,4 +1,4 @@
 set PATH=%CD%\gnutls-3.2.15-w32\bin;%CD%\Aspell-win32\bin;%PATH%
 REM set PATH=%CD%\gnutls-3.2.15-w32\bin;%PATH%
 REM start emacs-24.3\bin\runemacs.exe -l init.el %1
-start emacs-24.3\bin\runemacs.exe
+start emacs\bin\runemacs.exe
